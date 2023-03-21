7a1279b0bc924808872518ee870b9d61764f65c5
Marcus Holje <99866281+norskemarcus@users.noreply.github.com>
2023-03-21 12:38:46 +0100

    PROJ-15 added signup function

--- pages/signup/signup.js
+++ pages/signup/signup.js
@@ -1,34 +1,26 @@
-import { API_URL, FETCH_NO_API_ERROR } from "../../settings.js"
-import { handleHttpErrors } from "../../utils.js"
+import { API_URL, FETCH_NO_API_ERROR } from "../../settings.js";
+import { handleHttpErrors } from "../../utils.js";
 
-const URL = API_URL + "/members"
+const URL = API_URL + "/members";
 
-let inputUsername
-let inputEmail
-let inputPassword
-let inputFirstName
-let inputLastName
-let inputStreet
-let inputCity
-let inputZip
+let inputUsername;
+let inputEmail;
+let inputPassword;
+let inputFirstName;
 
 export function initSignup() {
   //Initialize nodes used more than once
-  inputUsername = document.getElementById("input-username")
-  inputEmail = document.getElementById("input-email")
-  inputPassword = document.getElementById("input-password")
-  inputFirstName = document.getElementById("input-firstname")
-  inputLastName = document.getElementById("input-lastname")
-  inputStreet = document.getElementById("input-street")
-  inputCity = document.getElementById("input-city")
-  inputZip = document.getElementById("input-zip")
+  inputUsername = document.getElementById("input-username");
+  inputEmail = document.getElementById("input-email");
+  inputPassword = document.getElementById("input-password");
+  inputFirstName = document.getElementById("input-firstname");
 
-  document.getElementById("form").onsubmit = saveMember
+  document.getElementById("form").onsubmit = saveMember;
   document.getElementById("goto-login").onclick = () => {
-    document.getElementById("goto-login").style.display = "none"
-  }
-  clearInputFields()
-  setStatusMsg("")
+    document.getElementById("goto-login").style.display = "none";
+  };
+  clearInputFields();
+  setStatusMsg("");
 }
 
 /**
@@ -37,54 +29,44 @@ export function initSignup() {
  * @param {boolean} [isError] true, to style in red
  */
 function setStatusMsg(msg, isError) {
-  const color = isError ? "red" : "darkgreen"
-  const statusNode = document.getElementById("status")
-  statusNode.style.color = color
-  statusNode.innerText = msg
+  const color = isError ? "red" : "darkgreen";
+  const statusNode = document.getElementById("status");
+  statusNode.style.color = color;
+  statusNode.innerText = msg;
 }
 
 function clearInputFields() {
-  inputUsername.value = ""
-  inputEmail.value = ""
-  inputPassword.value = ""
-  inputFirstName.value = ""
-  inputLastName.value = ""
-  inputStreet.value = ""
-  inputCity.value = ""
-  inputZip.value = ""
+  inputUsername.value = "";
+  inputEmail.value = "";
+  inputPassword.value = "";
+  inputFirstName.value = "";
 }
 
 async function saveMember(evt) {
-  evt.preventDefault()
-  setStatusMsg("")
+  evt.preventDefault();
+  setStatusMsg("");
   //Create the DTO object that must be sent with the body in the POST Request
-  const memberRequest = {}
-  memberRequest.username = inputUsername.value
-  memberRequest.email = inputEmail.value
-  memberRequest.password = inputPassword.value
-  memberRequest.firstName = inputFirstName.value
-  memberRequest.lastName = inputLastName.value
-  memberRequest.street = inputStreet.value
-  memberRequest.city = inputCity.value
-  memberRequest.zip = inputZip.value
+  const memberRequest = {};
+  memberRequest.username = inputUsername.value;
+  memberRequest.email = inputEmail.value;
+  memberRequest.password = inputPassword.value;
+  memberRequest.firstName = inputFirstName.value;
 
-  const postOptions = {}
-  postOptions.method = "POST"
-  postOptions.headers = { "Content-type": "application/json" }
+  const postOptions = {};
+  postOptions.method = "POST";
+  postOptions.headers = { "Content-type": "application/json" };
 
-  postOptions.body = JSON.stringify(memberRequest)
+  postOptions.body = JSON.stringify(memberRequest);
   try {
-    const newMember = await fetch(URL, postOptions).then(handleHttpErrors)
-    clearInputFields()
-    setStatusMsg(`Successfully created member "${newMember.username}"`)
-    document.getElementById("goto-login").style.display = "block"
-
+    const newMember = await fetch(URL, postOptions).then(handleHttpErrors);
+    clearInputFields();
+    console.log("Kommer vi hit?");
+    setStatusMsg(`Successfully created member "${newMember.username}"`);
+    //document.getElementById("goto-login").style.display = "block";
   } catch (err) {
-    setStatusMsg(err.message + FETCH_NO_API_ERROR, true)
+    setStatusMsg(err.message + FETCH_NO_API_ERROR, true);
     if (err.apiError) {
-      setStatusMsg(err.apiError.message, true)
+      setStatusMsg(err.apiError.message, true);
     }
-
   }
-
 }
